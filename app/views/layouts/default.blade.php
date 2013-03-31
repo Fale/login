9256266725f08b03b402ba55b9776c2221757532
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-03-31 22:15:36 +0200

    Improve layout

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
         <title>@yield('title')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Fabio Alessandro Locati">
-        <link href="style.css" rel="stylesheet">
+        <link href="styles/style.css" rel="stylesheet">
         <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     </style>
@@ -13,7 +13,7 @@
   <body>
     <div id="wrapper">
         <div id="header">
-            <p><img src="header_960.gif" width="960" height="140"></p>
+            <p><img src="images/logo.png" /></p>
         </div>
         <div class="container">
             @yield('content')
@@ -21,7 +21,7 @@
         <div id="footer">
             <table width="100%" border="0" cellspacing="0" cellpadding="3">
                 <tr>
-                    <td width="20%" style="vertical-align:middle"><img src="footer_logo.png" alt="Fermare il Declino" width="122" height="60" border="0"></td>
+                    <td width="20%" style="vertical-align:middle"><img src="images/footer_logo.png" alt="Fermare il Declino" width="122" height="60" border="0"></td>
                     <td width="15%"><a title="" href="http://www.fermareildeclino.it/cambiare-la-politica-fermare-il-declino-tornare-a-crescere" target="_blank">CHI SIAMO</a></td>
                     <td width="25%"><a title="" href="http://www.fermareildeclino.it/cambiare-la-politica-fermare-il-declino-tornare-a-crescere" target="_blank">IL NOSTRO MANIFESTO</a></td>
                     <td width="20%"><a title="" href="http://www.fermareildeclino.it/adesioni" target="_blank">I PRIMI FIRMATARI</a></td>
