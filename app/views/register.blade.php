7dd288039360129cf685fc32c1a4f0eb99d2b14f
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-01 12:57:13 +0200

    Enrich registration module

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -4,18 +4,45 @@
 
 @section('content')
     <h2>Registrati</h2>
-    {{ Form::open(array('url' => 'register', 'method' => 'POST')) }}
-        <!-- username field -->
-        <p>
-            {{ Form::label('email', 'Indirizzo e-mail') }}<br/>
-            {{ Form::text('email', Input::old('email')) }}
-        </p>
-        <!-- password field -->
-        <p>
-            {{ Form::label('password', 'Password') }}<br/>
-            {{ Form::password('password') }}
-        </p>
-        <!-- submit button -->
-        <p>{{ Form::submit('Registrati', array('class' => 'button')) }}</p>
+    {{ Form::open(array('url' => 'register', 'method' => 'POST', 'class' => 'form-horizontal')) }}
+        <div class="control-group">
+            {{ Form::label('nome', 'Nome', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('nome', Input::old('nome')) }}</div>
+        </div>
+        <div class="control-group">
+            {{ Form::label('cognome', 'Cognome', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('cognome', Input::old('cognome')) }}</div>
+        </div>
+        <div class="control-group">
+            {{ Form::label('citta', 'Città', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('citta', Input::old('citta')) }}</div>
+        </div>
+        <div class="control-group">
+            {{ Form::label('provincia', 'Provincia', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('provincia', Input::old('provincia')) }}</div>
+        </div>
+        <div class="control-group">
+            {{ Form::label('email', 'Indirizzo e-mail', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('email', Input::old('email')) }}</div>
+        </div>
+        <div class="control-group">
+            {{ Form::label('telefono', 'Telefono', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('telefono', Input::old('telefono')) }}</div>
+        </div>
+        <div class="control-group">
+            {{ Form::label('cf', 'Codice Fiscale', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('cf', Input::old('cf')) }}</div>
+        </div>
+        <div class="control-group">
+            {{ Form::label('password', 'Password', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('password') }}</div>
+        </div>
+        <div class="control-group">
+            {{ Form::label('password2', 'Ripeti Password', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('password2') }}</div>
+        </div>
+        <div class="control-group">
+            <div class="controls">{{ Form::submit('Registrati', array('class' => 'button')) }}</div>
+        </div>
     {{ Form::close() }}
 @stop
