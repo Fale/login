ed2ed008c736c7323f1a383c51c2819863113295
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-01 12:28:46 +0200

    Fix the guest decorator

--- app/filters.php
+++ app/filters.php
@@ -43,7 +43,7 @@ Route::filter('auth', function()
 Route::filter('guest', function()
 {
     if (Auth::check()) 
-        return Redirect::route('home')->with('flash_notice', 'You are already logged in!');
+        return Redirect::route('profile')->with('flash_notice', 'You are already logged in!');
 });
 
 /*
