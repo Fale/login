faf7db267d4c18b767d45c45bd7b09d869815bc0
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-02 14:25:46 +0200

    Add documents table

---
+++ app/database/migrations/2013_04_02_121529_create_documents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('documents', function($t) {
+            $t->increments('id');
+            $t->integer('user_id');
+            $t->integer('type');
+            $t->string('number');
+            $t->string('provider');
+            $t->timestamp('provided');
+            $t->timestamp('expiry');
+            $t->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('documents');
+	}
+
+}
