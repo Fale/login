260a7120c78f148002a581acf346d37b94f3485a
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-03 11:33:21 +0200

    Improve the layout of reset password

--- app/views/resetpassword.blade.php
+++ app/views/resetpassword.blade.php
@@ -1,18 +1,28 @@
 @extends('layouts.default')
 
-@section('title')Remind Password @stop
+@section('title')Reimposta la tua password @stop
 
 @section('content')
     @if (Session::has('error'))
         <div class="alert alert-error">{{ Session::get('error') }}</div>
     @endif
-    {{ Form::open(array('url' => 'resetpassword', 'method' => 'POST')) }}
-        <p>
-            {{ Form::text('token', $token) }}
-            {{ Form::label('email', 'Indirizzo e-mail') }}<br/>
-            {{ Form::text('email') }}
-            {{ Form::text('password') }}
-            {{ Form::text('password_confirmation') }}
-        <p>{{ Form::submit('Nuova password', array('class' => 'button')) }}</p>
+    <h2>Reimposta la tua password</h2>
+    {{ Form::open(array('url' => 'resetpassword', 'method' => 'POST', 'class' => 'form-horizontal')) }}
+        <div class="control-group">
+            {{ Form::label('email', 'Indirizzo e-mail', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('email') }}</div>
+        </div>
+        <div class="control-group">
+            {{ Form::label('password', 'Password', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('password') }}</div>
+        </div>
+        <div class="control-group">
+            {{ Form::label('password_confirmation', 'Ripeti la password', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('password_confirmation') }}</div>
+        </div>
+        <div class="control-group">
+            <div class="controls">{{ Form::submit('Nuova password', array('class' => 'button')) }}</div>
+        </div>
+        {{ Form::text('token', $token, array('class' => 'hidden')) }}
     {{ Form::close() }}
 @stop
