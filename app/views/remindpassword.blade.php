458b811cfb8bbd800935950148b4ca1c14f37baa
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-03 11:20:59 +0200

    Improve layout for new password

--- app/views/remindpassword.blade.php
+++ app/views/remindpassword.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.default')
 
-@section('title')Remind Password @stop
+@section('title')Richiedi nuova password @stop
 
 @section('content')
     @if (Session::has('flash_error'))
         <div class="alert alert-error">{{ Session::get('flash_error') }}</div>
     @endif
-
-    {{ Form::open(array('url' => 'remindpassword', 'method' => 'POST')) }}
-        <p>
-            {{ Form::label('email', 'Indirizzo e-mail') }}<br/>
-            {{ Form::text('email', Input::old('email')) }}
-        <p>{{ Form::submit('Nuova password', array('class' => 'button')) }}</p>
+    <h2>Richiedi nuova passoword</h2>
+    {{ Form::open(array('url' => 'remindpassword', 'method' => 'POST', 'class' => 'form-horizontal')) }}
+        <div class="control-group">
+            {{ Form::label('email', 'Indirizzo e-mail', array('class' => 'control-label')) }}
+            <div class="controls">{{ Form::text('email', Input::old('email')) }}</div>
+        </div>
+        <div class="control-group">
+            <div class="controls">{{ Form::submit('Nuova password', array('class' => 'button')) }}</div>
+        </div>
     {{ Form::close() }}
 @stop
