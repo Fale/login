1733c5f3346fcd76ec3be648af04f7f2a206d008
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-14 13:26:28 +0100

    Add sidebar

--- app/views/account/profile.blade.php
+++ app/views/account/profile.blade.php
@@ -3,9 +3,11 @@
 @section('title')Profilo @stop
 
 @section('content')
-    <h2>Benvenuto "{{ Auth::user()->email }}"!</h2>
-    <!-- <p>Your user ID is: {{ Auth::user()->id }}</p> -->
-    <p>Sei stato autenticato. <br>
-       Vai alla <a href="http://comitati.fareinrete.org">Home</a>
-    </p>
+    <?php $user = UserInfo::find(Auth::user()->id); ?>
+    <div class="row">
+        <div class="span9">
+            <h2>Benvenuto {{$user->name}} {{$user->surname}}!</h2>
+        </div>
+        @include('account.sidebar')
+    </div>
 @stop
---
+++ app/views/account/sidebar.blade.php
@@ -0,0 +1,8 @@
+<div class="span3">
+    <div class="well sidebar-nav">
+        <ul class="nav nav-list">
+            <li><a href="{{URL::route('profile')}}">Modifica profilo</a></li>
+            <li><a href="{{URL::route('documents')}}">Gestisci documenti</a></li>
+        </ul>
+    </div>
+</div>
