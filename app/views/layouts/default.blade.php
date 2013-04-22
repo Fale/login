f440fc00e0267e32221e575b4bfcec6c7b7cf447
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-22 14:49:50 +0200

    Fix footer

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -33,10 +33,10 @@
             <table width="100%" border="0" cellspacing="0" cellpadding="3">
                 <tr>
                     <td width="20%" style="vertical-align:middle"><img src="{{ URL::asset('images/footer_logo.png') }}" alt="Fermare il Declino" width="122" height="60" border="0"></td>
-                    <td width="15%"><a title="" href="http://www.fermareildeclino.it/cambiare-la-politica-fermare-il-declino-tornare-a-crescere" target="_blank">CHI SIAMO</a></td>
+                    <td width="15%"><a title="" href="http://comitati.fareinrete.org/manifesto-2" target="_blank">CHI SIAMO</a></td>
                     <td width="25%"><a title="" href="http://www.fermareildeclino.it/cambiare-la-politica-fermare-il-declino-tornare-a-crescere" target="_blank">IL NOSTRO MANIFESTO</a></td>
                     <td width="20%"><a title="" href="" target="_blank"></a><a title="" href="http://comitati.fareinrete.org/le-10-proposte/" target="">PROPOSTE</a></td>
-                    <td width="20%"><a title="" href="" target="_blank"><a href="mailto:lqfbapptest@gmail.com">E-MAIL</a></td>
+                    <td width="20%"><a title="" href="" target="_blank"><a href="mailto:aiuto@fare2.it">E-MAIL</a></td>
                 </tr>
             </table>
         </div>
