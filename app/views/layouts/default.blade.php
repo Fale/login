28312d8be61a309b0fb256b185bdb11249c86a71
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-26 23:17:26 +0200

    Improve layout

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -10,8 +10,15 @@
         <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script type="text/javascript" src="{{ URL::asset('js/bootstrap-datepicker.js') }}"></script>
-    </style>
-  </head>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-40403885-1', 'fareinrete.it');
+            ga('send', 'pageview');
+        </script>
+    </head>
   <body>
     <div id="wrapper">
         <div id="header">
@@ -33,7 +40,7 @@
             <table width="100%" border="0" cellspacing="0" cellpadding="3">
                 <tr>
                     <td width="20%" style="vertical-align:middle"><img src="{{ URL::asset('images/footer_logo.png') }}" alt="Fermare il Declino" width="122" height="60" border="0"></td>
-                    <td width="15%"><a title="" href="http://www.fermareildeclino.it/cambiare-la-politica-fermare-il-declino-tornare-a-crescere" target="_blank">CHI SIAMO</a></td>
+                    <td width="15%"><a title="" href="http://fareinrete.it/chi-siamo" target="_blank">CHI SIAMO</a></td>
                     <td width="25%"><a title="" href="http://fareinrete.it/manifesto-2" target="_blank">IL NOSTRO MANIFESTO</a></td>
                     <td width="20%"><a title="" href="" target="_blank"></a><a title="" href="http://fareinrete.it/le-10-proposte/" target="">PROPOSTE</a></td>
                     <td width="20%"><a title="" href="" target="_blank"><a href="mailto:aiuto@fare2.it">E-MAIL</a></td>
