a305d598098a82e227cbcf95dc7b30ec2bf6cc67
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-05-05 13:34:27 +0200

    Better star thinking of viewing and edit distinclty

--- app/views/default/account/sidebar.blade.php
+++ app/views/default/account/sidebar.blade.php
@@ -1,6 +1,7 @@
 <div class="span3">
     <div class="well sidebar-nav">
         <ul class="nav nav-list">
+            <li><a href="{{URL::route('profile')}}">Visualizza profilo</a></li>
             <li><a href="{{URL::route('profile')}}">Modifica profilo</a></li>
             <li><a href="{{URL::route('profile.documents.index')}}">Gestisci documenti</a></li>
         </ul>
