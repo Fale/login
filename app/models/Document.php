e6b00a24b40d978a60a1d90f699bfb7e0afaf87b
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-05-05 13:26:34 +0200

    Nod profile/documents works properly

--- app/models/Document.php
+++ app/models/Document.php
@@ -10,4 +10,26 @@ class Document extends Eloquent {
         'provided' => array('required', 'date_format:d/m/Y'),
         'expiry' => array('required', 'date_format:d/m/Y')
 	);
+
+    public static function data($data, $return = 0)
+    {
+        if ($return)
+        {
+            $rsl = explode ('-',$data);
+            $rsl = array_reverse($rsl);
+            return implode($rsl,'/');
+        }
+        else
+        {
+            $rsl = explode ('/',$data);
+            $rsl = array_reverse($rsl);
+            return implode($rsl,'-');
+        }
+    }
+
+    // TODO: Use FK instead of this work-around
+    public function typeName()
+    {
+        return DocumentType::find($this->type)->name;
+    }
 }
