2ff2ad1e391b83617e65cb7eb81f767aff4e2197
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-05-05 13:47:28 +0200

    improve profile.sidebar layout

--- app/views/default/profile/sidebar.blade.php
+++ app/views/default/profile/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="span3">
     <div class="well sidebar-nav">
         <ul class="nav nav-list">
-            {{link_to_route('profile', 'Visualizza profilo')}}
-            {{link_to_route('profile', 'Modifica profilo')}}
-            {{link_to_route('profile.documents.index', 'Gestisci documenti')}}
-            {{link_to_route('logout', 'Logout')}}
+            <li>{{link_to_route('profile', 'Visualizza profilo')}}</li>
+            <li>{{link_to_route('profile', 'Modifica profilo')}}</li>
+            <li>{{link_to_route('profile.documents.index', 'Gestisci documenti')}}</li>
+            <li>{{link_to_route('logout', 'Logout')}}</li>
         </ul>
     </div>
 </div>
