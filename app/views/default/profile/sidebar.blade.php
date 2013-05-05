fe02b4d796a1f9c72b31cebfd1672f870c9fdf6b
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-05-05 13:42:07 +0200

    Improve sidebar

--- app/views/default/profile/sidebar.blade.php
+++ app/views/default/profile/sidebar.blade.php
@@ -1,9 +1,10 @@
 <div class="span3">
     <div class="well sidebar-nav">
         <ul class="nav nav-list">
-            <li><a href="{{URL::route('profile')}}">Visualizza profilo</a></li>
-            <li><a href="{{URL::route('profile')}}">Modifica profilo</a></li>
-            <li><a href="{{URL::route('profile.documents.index')}}">Gestisci documenti</a></li>
+            {{link_to_route('profile', 'Visualizza profilo')}}
+            {{link_to_route('profile', 'Modifica profilo')}}
+            {{link_to_route('profile.documents.index', 'Gestisci documenti')}}
+            {{link_to_route('logout', 'Logout')}}
         </ul>
     </div>
 </div>
