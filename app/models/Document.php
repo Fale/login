f96b896f2831e270bec6edf766acacdc5df86e4c
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-05-05 13:18:15 +0200

    improved validation rules for documents

--- app/models/Document.php
+++ app/models/Document.php
@@ -4,9 +4,10 @@ class Document extends Eloquent {
     protected $guarded = array();
 
     public static $rules = array(
-		'type' => 'required',
-		'provider' => 'required',
-		'hidden' => 'required',
-		'expiry' => 'required'
+        'type' => 'required',
+        'number' => 'required',
+        'provider' => 'required',
+        'provided' => array('required', 'date_format:d/m/Y'),
+        'expiry' => array('required', 'date_format:d/m/Y')
 	);
 }
