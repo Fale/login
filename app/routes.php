e3566260d1a4868cee04489216d8deb96bcca6df
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-05-06 22:38:17 +0200

    Better putting all in the group, even if it's a work-around

--- app/routes.php
+++ app/routes.php
@@ -18,12 +18,13 @@ Route::group(array('before' => 'auth'), function()
     ));
 });
 
-// FIXME: work-around fo bug https://github.com/laravel/framework/commit/6b2de61c4cd86e707b09edca94f39b73e0060b89#commitcomment-3133249
-Route::get('/', array('as' => 'login', function () { return View::make(t('account.login')); }))->before('guest');
 
 // Unauthenticated stuff
 Route::group(array('before' => 'guest'), function()
 {
+    // FIXME: work-around fo bug https://github.com/laravel/framework/commit/6b2de61c4cd86e707b09edca94f39b73e0060b89#commitcomment-3133249
+    Route::get('/', array('as' => 'login', function () { return View::make(t('account.login')); }));
+
     //Autentication
     Route::controller('/', 'AccountController', Array(
         'getRegister' => 'register',
